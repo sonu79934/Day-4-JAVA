421721f167e9220cf5df8b70d560941b656059c8
sonu79934 <yogasritha8@gmail.com>
2024-12-05 16:22:45 +0530

    Add files via upload

---
+++ naturalnum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class naturalnum{
+    public static void main(String[] args){
+    //solution 2
+    int n=10;
+    int i=n-1;
+    while(i>=0)
+    {
+        System.out.println(n-i);
+        i-=1;
+    }
+    }
+} 
